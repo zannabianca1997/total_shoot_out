8fa0ec6aacd62922b138643dba43cdd0f6aa0d46
zannabianca1997 <zannabianca199712@gmail.com>
2016-01-31 17:56:19 +0100

    added shebang and todo, polished .gitignore

--- Main.py
+++ Main.py
@@ -4,17 +4,16 @@ import pygame
 from Game import Game
 from CONSTS import *
 
-#inizializing pygame
+# inizializing pygame
 pygame.init()
 
 game = Game()
 
-#TODO: init menu
+# TODO: init menu
 if DEBUG:
-    conf = {"nav1" : (1, "zannaShip"),"nav2" : (1, "yourShip"), "background" : 6}
+    conf = {"nav1": (1, "zannaShip"), "nav2": (1, "yourShip"), "background": 6}
 else:
-    conf = {"nav1" : (0, "zannaShip"),"nav2" : ( 0, "yourShip"), "background" : 6}
+    conf = {"nav1": (0, "zannaShip"), "nav2": (0, "yourShip"), "background": 6}
 game.Play(conf)
 
 pygame.quit()
-
